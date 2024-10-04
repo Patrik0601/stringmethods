eb81dd19b751d345628ef6a00c3d4be1c4061985
Patrick0601 <100844354+Patrik0601@users.noreply.github.com>
2024-10-04 11:36:46 +0200

    Add files via upload

---
+++ stringmethodus.js
@@ -0,0 +1,10 @@
+let text = "I am the storm that is approaching Provoking black clouds in isolation I am reclaimer of my name Born in flames, I have been blessed My family crest is a demon of death Forsakened, I am awakened A phoenixs ash in dark divine Descending misery Destiny chasing time"
+
+console.log(text.slice(0, 5))
+console.log(text.slice(3, 8))
+console.log(text.slice(5, text.length))
+console.log(text.slice(5, 5+6))
+console.log(text.toUpperCase())
+
+console.log(text.replaceAll("e", "E"))
+console.log(text.replaceAll("e", " e "))
